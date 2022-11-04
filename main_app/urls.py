68297a090c7f094614a3f58db3a140f7e04b0d3a
julianboyne <julianboyne11@gmail.com>
2022-11-04 19:40:02 -0400

    implement index view and template for super hero

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
   path('', views.home, name='home'),
-    path('about/', views.about, name='about'),
+  path('about/', views.about, name='about'),
+  path('super-heros/', views.heros_index, name='heros_index')
+
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -2,8 +2,24 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+# Add the Cat class & list and view function below the imports
+class Hero:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, name, universe, description,):
+    self.name = name
+    self.universe = universe
+    self.description = description
+
+heros = [
+  Hero('Spiderman', 'Marvel', 'Spiderman, does whatever spider can'),
+  Hero('Iron Man', 'Marvel', 'Tony Stark is and great innovator'),
+]
+
 def home(request):
   return HttpResponse('<h1>Hello There</h1>')
 
 def about(request):
   return render(request, 'about.html')
+
+def heros_index(request):
+  return render(request, 'heros/index.html', {'heros': heros})
+
