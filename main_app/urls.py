a8f0a5720deb5b63c735095e2c6ccddf51819c29
julianboyne <julianboyne11@gmail.com>
2022-11-05 21:30:53 -0400

    implement home template and superhero model

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,6 +4,5 @@ from . import views
 urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
-  path('super-heros/', views.heros_index, name='heros_index')
-
+  path('superheros/', views.heros_index, name='heros_index')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,25 +1,14 @@
 from django.shortcuts import render
+from .models import Superhero
 
-from django.http import HttpResponse
-
-# Add the Cat class & list and view function below the imports
-class Hero:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, universe, description,):
-    self.name = name
-    self.universe = universe
-    self.description = description
-
-heros = [
-  Hero('Spiderman', 'Marvel', 'Spiderman, does whatever spider can'),
-  Hero('Iron Man', 'Marvel', 'Tony Stark is and great innovator'),
-]
 
 def home(request):
-  return HttpResponse('<h1>Hello There</h1>')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
 
 def heros_index(request):
+  heros = Superhero.objects.all()
   return render(request, 'heros/index.html', {'heros': heros})
 
