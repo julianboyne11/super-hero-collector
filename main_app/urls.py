c5f0a8ce81d50f260ec44df561aeed5dd0eab61a
julianboyne <julianboyne11@gmail.com>
2022-11-05 22:47:02 -0400

    implement super hero template and view

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
-  path('superheros/', views.heros_index, name='heros_index')
+  path('superheros/', views.heros_index, name='heros_index'),
+  path('superheros/<int:hero_id>', views.superhero_detail, name='superhero_detail'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -12,3 +12,6 @@ def heros_index(request):
   heros = Superhero.objects.all()
   return render(request, 'heros/index.html', {'heros': heros})
 
+def superhero_detail(request, hero_id):
+  hero = Superhero.objects.get(id=hero_id)
+  return render(request, 'heros/detail.html', { 'hero': hero })
