bc7feee3a9a5ef93587c8de3a00fa0e1f22853c0
julianboyne <julianboyne11@gmail.com>
2022-11-07 18:04:19 -0500

    implemente delete and update superhero functionality and template

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
   path('superheros/', views.heros_index, name='heros_index'),
   path('superheros/<int:hero_id>', views.superhero_detail, name='superhero_detail'),
   path('superheros/create/', views.HeroCreate.as_view(), name='superhero_create'),
+  path('supeheros/<int:pk>/update', views.HeroUpdate.as_view(), name='superhero_update'),
+  path('supeheros/<int:pk>/delete', views.HeroDelete.as_view(), name='superhero_delete'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic.edit import CreateView
+# Add UdpateView & DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Superhero
 
 
@@ -20,3 +21,11 @@ def superhero_detail(request, hero_id):
 class HeroCreate(CreateView):
   model = Superhero
   fields = '__all__'
+
+class HeroUpdate(UpdateView):
+  model = Superhero
+  fields = ['universe', 'description']
+
+class HeroDelete(DeleteView):
+  model =Superhero
+  success_url = '/superheros/'
