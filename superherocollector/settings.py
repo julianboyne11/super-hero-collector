36177286d8cf9c30df029b4a79b272e4f0033c9c
julianboyne <julianboyne11@gmail.com>
2022-11-14 10:34:45 -0500

    update env

--- superherocollector/settings.py
+++ superherocollector/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8o$ic!4y5-l7)7%2_@v5h5$3u77h-nkwbg()+kg1hbl6nf)g%_'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
